efc25d8e1aedf4635b3da8451763add5863086fc
Pano Valdez <marianovaldez92@protonmail.com>
2024-09-18 15:10:52 -0300

    limpieza de archivos

--- letsGoUp.sh
+++
@@ -1,9 +0,0 @@
-# Arriba el docker de mongo
-docker ps -a | grep mongodb-der | docker start mongodb-der || docker run --name "mongodb-der" -p 27017:27017 --volume `pwd`/tmp/db:/data/db -w /data/db -d mongo:3.2
-
-# Uso node v8
-source ~/.nvm/nvm.sh
-nvm use v8.17.0
-
-# Arriba react!
-NODE_PATH=. DEBUG=democracyos* ./node_modules/.bin/gulp bws

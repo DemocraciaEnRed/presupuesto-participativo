a9fba411109dfa2349ae2b6c191bbf28b6ef4ee9
Pano Valdez <marianovaldez92@protonmail.com>
2024-09-18 15:12:08 -0300

    corrijo run-dev

--- run-dev.sh
+++ run-dev.sh
@@ -1,12 +1,9 @@
-# si docker no está prendido, prenderlo
-pgrep dockerd > /dev/null || sudo systemctl start docker
+# Arriba el docker de mongo
+docker ps -a | grep mongodb-der | docker start mongodb-der || docker run --name "mongodb-der" -p 27017:27017 --volume `pwd`/tmp/db:/data/db -w /data/db -d mongo:3.2
 
-# si el container no está corriendo, arrancarlo
-docker inspect --format="{{.State.Status}}" mongodb-mgp | grep -q running || docker start mongodb-mgp
+# Uso node v8
+source ~/.nvm/nvm.sh
+nvm use v8
 
-# si no estamos usando node v8, cambiar a esa
-node -v | grep -q v8 || nvm use v8
-
-# finalmente, levantar el sistema (bws=build watch serve)
-#NODE_PATH=. DEBUG=democracyos* gulp bws
+# Arriba react!
 NODE_PATH=. DEBUG=democracyos* ./node_modules/.bin/gulp bws
